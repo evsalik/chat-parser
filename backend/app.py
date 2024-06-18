2ec353bdb627b2571812a7cca507a243f9baf68a
evsalik <ems2312227@gmail.com>
2024-06-18 16:10:17 +0300

    trying

--- backend/app.py
+++ backend/app.py
@@ -245,5 +245,10 @@ def upload_file():
     else:
         return jsonify({"error": "Invalid file format"}), 400
 
+@app.route('/')
+def hello():
+    return "Backend is running"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
